dbd11250ef02886025aeb5ddc42727908aba9047
Arief <stefarf@gmail.com>
2018-04-17 13:13:03 +0700

    add run command example: demonstrate calling mysql client to execute some sql command

---
+++ run_cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os/exec"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+const (
+	mysql    = "c:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysql.exe"
+	rootPass = "root"
+)
+
+func main() {
+	f, err := os.Open("create.sql")
+	panicIf(err)
+	defer f.Close()
+
+	cmd := exec.Command(mysql, "-u", "root", "-p"+rootPass)
+	cmd.Stdin = f
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	panicIf(cmd.Run())
+
+	fmt.Printf("Output: ###\n%s\n###\n", out.String())
+}
+
+func panicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
