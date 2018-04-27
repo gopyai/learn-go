304250e0bbaf1b269a06aec5975e360283f63d72
Arief <stefarf@gmail.com>
2018-04-27 19:32:10 +0700

    Add binary-only-package examples.

---
+++ bin_only_package/lib/lib.go
@@ -0,0 +1,11 @@
+//go:binary-only-package
+
+package lib
+
+import (
+	"fmt"
+)
+
+func Run() {
+	fmt.Println("Hello binary")
+}
---
+++ bin_only_package/main.go
@@ -0,0 +1,11 @@
+// Currently it does not work on go 1.10.
+// Tested on go 1.9.5 instead.
+package main
+
+import (
+	"learn-go/bin_only_package/lib"
+)
+
+func main() {
+	lib.Run()
+}
